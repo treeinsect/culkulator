077c326882b3f016dddacfcef149016cc0315b17
treeinsect <swright9332@gmail.com>
2024-09-09 22:52:38 +0100

    fixed duplicating results

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.querySelector('#button').addEventListener('click',countFilms);
 
 function countFilms(){
 
+    //init variables
     var count = 0;
     var percent = 0;
 
+    //go through the checkboxes and see if they are checked
     for (let i = 1; i < 5; i++){
         var checkbox = document.getElementById(i);
         if (checkbox.checked == true){
@@ -13,11 +15,14 @@ function countFilms(){
         percent = (count/i)*100;
     }
 
-    console.log(percent);
-
+    //display the % of boxes checked to the user
     const result = document.getElementById("result");
+    while (result.firstChild){
+        result.removeChild(result.firstChild);
+    }
     const node = document.createTextNode(percent+"% watched");
     result.appendChild(node);
+
     return;
 
 }
