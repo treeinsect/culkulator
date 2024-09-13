5f3398eb8016fba8f4f1a718aec94f0053971b34
treeinsect <swright9332@gmail.com>
2024-09-13 19:08:47 +0100

    fixed countFilms to loop properly

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function countFilms(){
     //init variables
     var count = 0;
     var percent = 0;
-    var lenFilms = document.querySelector("#films").getElementsByTagName("li").length;
+    const lenFilms = document.querySelector("#films").getElementsByTagName("li").length;
 
     //go through the checkboxes and see if they are checked
-    for (let i = 1; i = lenFilms; i++){
+    for (let i = 1; i <= lenFilms; i++){
         var checkbox = document.getElementById(i);
         if (checkbox.checked == true){
             count++;
